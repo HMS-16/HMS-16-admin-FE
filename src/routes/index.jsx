c7fdd47fff8cfce58f9dcea116e12d512e0dac97
Rotartsinimda <rio.rasyid23@gmail.com>
2022-12-14 17:12:33 +0700

    feature: LandingPage

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,20 +6,23 @@ import ManageNurse from "../pages/ManageNurse";
 import ManagePatient from "../pages/ManagePatient";
 import Appointment from "../pages/Appointment";
 import Sidebar from "../components/Sidebar";
+import LandingPage from "../pages/LandingPage";
 
 const Routers = () => {
   return (
     <BrowserRouter>
-      <Sidebar>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/patient" element={<ManagePatient />} />
-          <Route path="/doctor" element={<ManageDoctor />} />
-          <Route path="/nurse" element={<ManageNurse />} />
-          <Route path="/appointment" element={<Appointment />} />
-        </Routes>
-      </Sidebar>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/patient" element={<ManagePatient />} />
+        <Route path="/doctor" element={<ManageDoctor />} />
+        <Route path="/nurse" element={<ManageNurse />} />
+        <Route path="/appointment" element={<Appointment />} />
+      </Routes>
+
+      {/* <Routes>
+        <Route path="/landing" element={<LandingPage />} />
+      </Routes> */}
     </BrowserRouter>
   );
 };
